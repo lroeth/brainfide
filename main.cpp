da84c7a09adf97cf10d155e7091f82179d4f7898
Liam Roeth <lroeth1@pride.hofstra.edu>
2021-04-27 18:08:35 -0400

    use Fl_Packs instead of hand positioning
    Instead of manually positioning and sizing each widget in Fl_Groups,
      let Fl_Packs do the work
    Set appropriate window size restriction

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <FL/Fl.H>
-#include <FL/Fl_Group.H>
+#include <FL/Fl_Pack.H>
 #include <FL/Fl_Window.H>
 #include <FL/Fl_Text_Editor.H>
 #include <FL/Fl_Text_Display.H>
@@ -13,34 +13,41 @@
 #define W_BUTTON 100
 #define H_BUTTON 40
 #define H_DISP 100
-
+#define MIN_W_EDITOR 100
 
 /* convenience macros */
-/* TODO: use Fl_Packs instead of hand positioning */
 #define W_EDITOR W_WINDOW - W_BUTTON
 #define H_EDITOR H_WINDOW - H_DISP
 #define W_DISP W_WINDOW
-#define NUM_BUTTONS 2
 
 int main(int argc, char **argv)
 {
   /* widget hierarchy */
   Fl_Window *window = new Fl_Window(W_WINDOW,H_WINDOW);
-    Fl_Group *groupResize = new Fl_Group(0,0,W_EDITOR,H_EDITOR);
-      Fl_Text_Editor *editor = new Fl_Text_Editor(0,0,W_EDITOR,H_EDITOR);
-    groupResize->end();
-    Fl_Group *groupButtons = new Fl_Group(W_EDITOR,0,W_BUTTON,H_EDITOR);
-      Fl_Button *buttonRun = new Fl_Button(W_EDITOR,0,W_BUTTON,H_BUTTON,"Run");
-      Fl_Button *buttonBack = new Fl_Button(W_EDITOR,1 * H_BUTTON,W_BUTTON/2,H_BUTTON,"@<");
-      Fl_Button *buttonForward = new Fl_Button(W_EDITOR + W_BUTTON/2,1 * H_BUTTON,W_BUTTON/2,H_BUTTON,"@>");
-      Fl_Box *spaceButtons = new Fl_Box(W_EDITOR,NUM_BUTTONS * H_BUTTON,W_BUTTON,H_EDITOR - NUM_BUTTONS * H_BUTTON);
-    groupButtons->end();
-    Fl_Text_Display *dispIo = new Fl_Text_Display(0,H_EDITOR,W_DISP,H_DISP);
+    Fl_Pack *packWindow = new Fl_Pack(0,0,W_WINDOW,H_WINDOW);
+      Fl_Pack *packEditor = new Fl_Pack(0,0,W_WINDOW,H_EDITOR);
+        Fl_Text_Editor *editor = new Fl_Text_Editor(0,0,W_EDITOR,0);
+        Fl_Pack *packButtons = new Fl_Pack(0,0,W_BUTTON,0);
+          Fl_Button *buttonRun = new Fl_Button(0,0,0,H_BUTTON,"Run");
+          Fl_Pack *packStep = new Fl_Pack(0,0,0,H_BUTTON);
+            Fl_Button *buttonBack = new Fl_Button(0,0,W_BUTTON/2,0,"@<");
+            Fl_Button *buttonForward = new Fl_Button(0,0,W_BUTTON/2,0,"@>");
+          packStep->end();
+        packButtons->end();
+      packEditor->end();
+      Fl_Text_Display *dispIo = new Fl_Text_Display(0,H_EDITOR,W_DISP,H_DISP);
+    packWindow->end();
   window->end();
 
-  /* adjust widgets */
-  window->resizable(groupResize);
-  groupButtons->resizable(spaceButtons);
+  /* configure widgets and resize behaviour */
+  packWindow->type(Fl_Pack::VERTICAL);
+  packEditor->type(Fl_Pack::HORIZONTAL);
+  packButtons->type(Fl_Pack::VERTICAL);
+  packStep->type(Fl_Pack::HORIZONTAL);
+  packEditor->resizable(editor);
+  packWindow->resizable(packEditor);
+  window->resizable(packWindow);
+  window->size_range(MIN_W_EDITOR+W_BUTTON,packButtons->children()*H_BUTTON + H_DISP);
 
   /* setup data structures and callbacks */
   Fl_Text_Buffer *bufferProg = new Fl_Text_Buffer();

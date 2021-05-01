28418947fb0522cafae4d3ded94acef40d61816c
Liam Roeth <lroeth1@pride.hofstra.edu>
2021-05-01 00:47:41 -0400

    change return status of step

--- bfint.h
+++ bfint.h
@@ -8,7 +8,7 @@ class BFInt
     BFInt();
     bool update_program(std::string program);
     void reset_exec();
-    signed char step(); /* returns -1 for error stop, 1 for normal stop, 0 for continue */
+    signed char step(); /* returns -1 for error stop, 1 for eof, positive for d_handle status, 0 for continue */
 
   protected:
     unsigned get_prog_pos();
@@ -24,7 +24,7 @@ class BFInt
     virtual void err_output(std::string message, bool is_error)=0;
 
   private:
-    virtual signed char d_handle() {return -1;} /* return negative to indicate unhandled character, positive to stop, 0 to continue */
+    virtual signed char d_handle() {return -1;} /* return negative to indicate unhandled character, 1 for eof, positive to stop, 0 to continue */
     virtual void d_add_cell() {}
     virtual void d_write_cell(unsigned char val) {}
     virtual void d_write_tape_pos(unsigned oldPos) {}
